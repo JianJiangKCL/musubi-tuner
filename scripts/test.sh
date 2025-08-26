fae5a25376c7ec96ac48bfe403dfc9115955fa24
Yujia <yujia.xu.1113@gmail.com>
2025-08-27 03:47:02 +0800

    successful 1 hour peel it high noise lora train and test. model4infer.py created for inference

--- scripts/test.sh
+++ scripts/test.sh
@@ -35,18 +35,25 @@ CUDA_VISIBLE_DEVICES=7 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_gene
     /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/girl.jpg
 
 
-    CUDA_VISIBLE_DEVICES=5 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_generate_video.py \
+    CUDA_VISIBLE_DEVICES=6 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_generate_video.py \
     --dit  /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/low_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
     --dit_high_noise /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/high_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
     --lazy_loading \
     --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth \
     --task i2v-A14B \
-    --prompt "A hand peels a realistic 3D sticker of a car off a flat surface，revealing the flat background underneath and emphasizing the illusion of depth." \
+    --prompt "A hand peels a realistic 3D sticker of the main object off a flat surface，revealing the flat background underneath and emphasizing the illusion of depth." \
     --video_size 320 480 \
-    --video_length 81 \
+    --video_length 101 \
     --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth \
     --vae_cache_cpu \
     --save_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/ \
     --attn_mode flash2 \
-    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_full_trace8/peel_it-of-lorac.safetensors \
-    --image_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/test_car.jpg
+    --image_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/test_car.jpg \
+    --lora_weight_high_noise /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_trace20/peel_it-of-lorac.safetensors \
+    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_trace20/peel_it-of-lorac.safetensors 
+
+
+    --seed 22
+       \
+
+        
---
+++ scripts/train_i2v_trace20.sh
@@ -0,0 +1,35 @@
+#https://github.com/kohya-ss/musubi-tuner/blob/feat-wan-generate-lazy-loading/docs/wan.md
+# high noise
+CUDA_VISIBLE_DEVICES=6 accelerate launch --num_cpu_threads_per_process 1 --mixed_precision bf16 /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_train_network.py \
+    --dit /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/low_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --dit_high_noise /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/high_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/peel_it_i2v_meta_trace20.toml \
+    --flash_attn \
+    --mixed_precision bf16 \
+    --fp8_base \
+    --fp8_scaled \
+    --optimizer_type adamw \
+    --task i2v-A14B \
+    --offload_inactive_dit \
+    --vae_cache_cpu \
+    --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth \
+    --max_data_loader_n_workers 8 --persistent_data_loader_workers \
+    --network_module networks.lora_wan --network_dim 16 --network_alpha 16 \
+    --timestep_sampling shift --discrete_flow_shift 1.0  \
+    --max_train_epochs 10 --save_every_n_epochs 1 --seed 42 \
+    --output_dir /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_trace20 --output_name peel_it-of-lorac \
+    --preserve_distribution_shape \
+    --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth  \
+    --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth \
+    --optimizer_args weight_decay=0.1 \
+    --max_grad_norm 0 \
+    --learning_rate 3e-4  \
+    --lr_scheduler polynomial \
+    --lr_scheduler_power 1 \
+    --lr_scheduler_min_lr_ratio 0.433 \
+    --min_timestep 875 \
+    --max_timestep 1000 \
+    --gradient_checkpointing
+
+
+
