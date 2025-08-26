a724d9e901aabed9f25afae4fc07ba813700f5c7
Yujia <yujia.xu.1113@gmail.com>
2025-08-27 00:08:05 +0800

    Update scripts: caching.sh and test.sh

--- scripts/caching.sh
+++ scripts/caching.sh
@@ -9,6 +9,12 @@ CUDA_VISIBLE_DEVICES=3 python /mnt/cfs/jj/musubi-tuner/wan_cache_latents.py --da
 CUDA_VISIBLE_DEVICES=4 python /mnt/cfs/jj/musubi-tuner/wan_cache_text_encoder_outputs.py --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/baby_bee_i2v_meta_json.toml  --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 128 
 
 
+#### peel it
 CUDA_VISIBLE_DEVICES=5 python /mnt/cfs/jj/musubi-tuner/wan_cache_latents.py --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/peel_it_i2v_meta_json.toml --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth --vae_dtype bfloat16 --batch_size 180 --num_workers 8 --i2v
 
 CUDA_VISIBLE_DEVICES=4 python /mnt/cfs/jj/musubi-tuner/wan_cache_text_encoder_outputs.py --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/peel_it_i2v_meta_json.toml --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 128 
+
+## baby bee
+CUDA_VISIBLE_DEVICES=4 python /mnt/cfs/jj/musubi-tuner/wan_cache_latents.py --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/babybee_i2v_meta_trace7.toml --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth --vae_dtype bfloat16 --batch_size 180 --num_workers 8 --i2v
+
+CUDA_VISIBLE_DEVICES=4 python /mnt/cfs/jj/musubi-tuner/wan_cache_text_encoder_outputs.py --dataset_config /mnt/cfs/jj/musubi-tuner/my_config/babybee_i2v_meta_trace7.toml --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth  --batch_size 128 
--- scripts/test.sh
+++ scripts/test.sh
@@ -5,15 +5,48 @@ CUDA_VISIBLE_DEVICES=6 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_gene
     --lazy_loading \
     --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth \
     --task i2v-A14B \
-    --prompt "A realistic sticker of a woman is peeled off a surface vertically, revealing the flat background underneath and emphasizing the illusion of depth." \
+    --prompt "A hand peels a realistic 3D sticker of a car off a flat surface，revealing the flat background underneath and emphasizing the illusion of depth." \
     --video_size 320 480 \
     --video_length 81 \
     --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth \
     --vae_cache_cpu \
     --save_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/ \
     --attn_mode flash2 \
-    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_frame81_uniform/peel_it-of-lorac-000050.safetensors \
-    --image_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/shang.jpg
-
+    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_frame81_uniform/peel_it-of-lorac-000150.safetensors \
+    --image_path /mnt/cfs/jj/musubi-tuner/datasets/peel_it/first_frames/1.jpg
 
 # CUDA_VISIBLE_DEVICES=1  python /mnt/cfs/jj/Wan2.2/generate.py  --task t2v-A14B --size 1280*720 --ckpt_dir /mnt/cfs/jj/ckpt/Wan2.2-T2V-A14B --offload_model True --convert_model_dtype --prompt "A cute baby bee flying in a flower garden"
+
+CUDA_VISIBLE_DEVICES=7 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_generate_video.py \
+    --dit  /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/low_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --dit_high_noise /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/high_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --lazy_loading \
+    --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth \
+    --task i2v-A14B \
+    --prompt "A hand peels a realistic 3D sticker of a woman off a flat surface, revealing the blank background underneath and emphasizing the illusion of depth." \
+    --video_size 320 480 \
+    --video_length 81 \
+    --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth \
+    --vae_cache_cpu \
+    --save_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/ \
+    --attn_mode flash2 \
+    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_full_trace8/peel_it-of-lorac.safetensors \
+    --image_path /mnt/cfs/jj/musubi-tuner/datasets/peel_it/first_frames/2.jpg
+    /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/girl.jpg
+
+
+    CUDA_VISIBLE_DEVICES=5 python /mnt/cfs/jj/musubi-tuner/src/musubi_tuner/wan_generate_video.py \
+    --dit  /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/low_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --dit_high_noise /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/high_noise_model/diffusion_pytorch_model-00001-of-00006.safetensors \
+    --lazy_loading \
+    --t5 /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/models_t5_umt5-xxl-enc-bf16.pth \
+    --task i2v-A14B \
+    --prompt "A hand peels a realistic 3D sticker of a car off a flat surface，revealing the flat background underneath and emphasizing the illusion of depth." \
+    --video_size 320 480 \
+    --video_length 81 \
+    --vae /mnt/cfs/jj/ckpt/Wan2.2-I2V-A14B/Wan2.1_VAE.pth \
+    --vae_cache_cpu \
+    --save_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/ \
+    --attn_mode flash2 \
+    --lora_weight /mnt/cfs/jj/musubi-tuner/peel_it_outputs_high_noise_full_trace8/peel_it-of-lorac.safetensors \
+    --image_path /mnt/cfs/jj/musubi-tuner/test_i2v_outputs/test_car.jpg
